9491b32871a9c61bb20f293bbb5d12e3a9cd9266
Joelle Gross <50854878+joellegross@users.noreply.github.com>
2024-08-02 12:58:38 -0400

    added a new folder and a blank player!

---
+++ src/words/g6/g6PlayerV1.java
@@ -0,0 +1,17 @@
+package words.g6;
+
+import words.core.Letter;
+import words.core.Player;
+import words.core.PlayerBids;
+import words.core.SecretState;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class g6PlayerV1 extends Player{
+
+   
+
+
+}
